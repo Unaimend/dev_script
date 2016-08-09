8aaa2afa878e04945103c9bf484be3fe698df734
tdOSX <Unaimend@gmail.com>
2016-08-09 12:22:37 +0200

    First commit

---
+++ webdev.sh
@@ -0,0 +1,51 @@
+#Author: Thomas Dost
+
+
+if (( "$#" == 0 )) 
+	then 
+		
+		echo "Pls enter a project name"
+		read -p "Enter: " name
+		name=${name}
+	else
+		#echo "YAY"
+		#echo $1
+		name=${1}
+fi
+#echo $name
+mkdir $name
+cd $name
+
+mkdir html
+mkdir javascript
+
+echo "
+/*************************
+*Author: 	Thomas Dost
+*Purpose:	
+*Date:
+*Changelog:	
+*Todo:
+**************************/
+
+Kopiervorlage:
+
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script type="text/javascript" src="javascript/index.js"></script>
+    <title>Titel</title>
+  </head>
+  <body>
+  </body>
+</html>
+" >> html/index.html
+
+
+echo"
+alert(\"Hello World\");
+" >> javascript/index.js
+
+
