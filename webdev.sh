f6b701c3d4ed4e2c5867af1ad41fcf9d5a3085d7
tdOSX <Unaimend@gmail.com>
2016-11-14 16:57:53 +0100

    Fixed js source path

--- webdev.sh
+++ webdev.sh
@@ -33,7 +33,7 @@ echo "
   <head>
     <meta charset=\"utf-8\">
     <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
-    <script type=\"text/javascript\" src=\"javascript/index.js\"></script>
+    <script type=\"text/javascript\" src=\"../javascript/index.js\"></script>
     <title>Titel</title>
   </head>
   <body>
