8a764d5cfac4ac2229daab12259c97af20605065
tdOSX <Unaimend@gmail.com>
2017-05-06 15:18:55 +0200

    File aufm Pytho3 geupdated

--- dev_script_gui.py
+++ dev_script_gui.py
@@ -1,30 +1,30 @@
-from Tkinter import *
-import tkFileDialog
+import tkinter as tk
+from tkinter import filedialog
 # Uses PEP8 style guide
 
 
 class Gui(object):
     def __init__(self, root):
         self.root = root
-        self.var1 = BooleanVar()
-        self.var2 = BooleanVar()
+        self.var1 = tk.BooleanVar()
+        self.var2 = tk.BooleanVar()
         self.dir_opt = {}
         self.path = 0
 
-        self.download_sfml_box = Checkbutton(
+        self.download_sfml_box = tk.Checkbutton(
             root, text="SFML", variable=self.var1).grid(row=0)
-        self.download_lua_box = Checkbutton(
+        self.download_lua_box = tk.Checkbutton(
             root, text="Lua", variable=self.var2).grid(row=0, column=1)
-        self.go_button = Button(root, text="GO").grid(row=2)
-        self.exit_button = Button(
+        self.go_button = tk.Button(root, text="GO").grid(row=2)
+        self.exit_button = tk.Button(
             root, text="Exit", command=root.quit).grid(row=2, column=1)
-        self.where_button = Button(
+        self.where_button = tk.Button(
             root, text="Where?", command=self.ask_dir).grid(row=2, column=2)
     # Checkbutton(root, text="3", variable=var1).grid(rod=3, sticky=d)
 
     def ask_dir(self):
         self.dir_opt['title'] = 'Please select directory'
-        self.path = tkFileDialog.askdirectory(**self.dir_opt)
+        self.path = tk.filedialog.askdirectory(**self.dir_opt)
         if not self.path:
             return
 
@@ -32,6 +32,6 @@ class Gui(object):
         self.root.mainloop()
 
 
-GUI = Gui(Tk())
+GUI = Gui(tk.Tk())
 GUI.run()
-print GUI.path
+print(GUI.path)
