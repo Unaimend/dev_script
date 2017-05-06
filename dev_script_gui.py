8928a50cfc4d083d891cf5c444e25e32ef629a4c
tdOSX <Unaimend@gmail.com>
2017-05-06 14:19:40 +0200

    autopep8 benutzt

--- dev_script_gui.py
+++ dev_script_gui.py
@@ -1,7 +1,6 @@
 from Tkinter import *
 import tkFileDialog
-#Uses PEP8 style guide
-
+# Uses PEP8 style guide
 
 
 class Gui(object):
@@ -12,12 +11,15 @@ class Gui(object):
         self.dir_opt = {}
         self.path = 0
 
-        self.download_sfml_box = Checkbutton(root, text="SFML", variable=self.var1).grid(row=0)
+        self.download_sfml_box = Checkbutton(
+            root, text="SFML", variable=self.var1).grid(row=0)
         self.download_lua_box = Checkbutton(
             root, text="Lua", variable=self.var2).grid(row=0, column=1)
         self.go_button = Button(root, text="GO").grid(row=2)
-        self.exit_button = Button(root, text="Exit", command=root.quit).grid(row=2, column=1)
-        self.where_button = Button(root, text="Where?", command=self.ask_dir).grid(row=2, column=2)
+        self.exit_button = Button(
+            root, text="Exit", command=root.quit).grid(row=2, column=1)
+        self.where_button = Button(
+            root, text="Where?", command=self.ask_dir).grid(row=2, column=2)
     # Checkbutton(root, text="3", variable=var1).grid(rod=3, sticky=d)
 
     def ask_dir(self):
